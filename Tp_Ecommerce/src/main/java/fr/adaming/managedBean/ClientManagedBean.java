607b05028e146de22f4e2feacc819070828953f0
Adaming <Adaming@DESKTOP-8GMSH3M>
2018-11-29 11:42:40 +0100

    Cyrille: commit setter clService

--- Tp_Ecommerce/src/main/java/fr/adaming/managedBean/ClientManagedBean.java
+++ Tp_Ecommerce/src/main/java/fr/adaming/managedBean/ClientManagedBean.java
@@ -1,4 +1,5 @@
 package fr.adaming.managedBean;
+
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
@@ -14,68 +15,68 @@ import fr.adaming.service.IClientService;
 @SessionScoped
 public class ClientManagedBean {
 	// Injection de dépendance des services
-		@ManagedProperty(value = "#{clService}")
-		private IClientService clService;
-		
-		
-		private Client client;
-		private boolean logInCl;
+	@ManagedProperty(value = "#{clService}")
+	private IClientService clService;
 
+	public void setClService(IClientService clService) {
+		this.clService = clService;
+	}
 
+	private Client client;
+	private boolean logInCl;
 
-		public ClientManagedBean() {
-			super();
-			
-		}
-		
-@PostConstruct
-public void init() {
-	this.client=new Client();
-}
+	public ClientManagedBean() {
+		super();
 
-public Client getClient() {
-	return client;
-}
+	}
 
-public void setClient(Client client) {
-	this.client = client;
-}
+	@PostConstruct
+	public void init() {
+		this.client = new Client();
+	}
 
-public boolean isLogInCl() {
-	return logInCl;
-}
+	public Client getClient() {
+		return client;
+	}
 
-public void setLogInCl(boolean logInCl) {
-	this.logInCl = logInCl;
-}
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public boolean isLogInCl() {
+		return logInCl;
+	}
+
+	public void setLogInCl(boolean logInCl) {
+		this.logInCl = logInCl;
+	}
 
 // Les méthodes
-public String login() {
-	System.out.println(client.getMail());
-	System.out.println(client.getPassword());
-	try {
-		Client clOut = clService.isExist(this.client);
+	public String login() {
+		System.out.println(client.getMail());
+		System.out.println(client.getPassword());
+		try {
+			Client clOut = clService.isExist(this.client);
 
-		// Le mettre dans la session
-		FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("clSession", clOut);
+			// Le mettre dans la session
+			FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("clSession", clOut);
 
-		// Mettre la liste des catégories et des produits dans la session
-		FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("Commande",
-				clOut.getpCommandes());
+			// Mettre la liste des catégories et des produits dans la session
+			FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("Commande",
+					clOut.getpCommandes());
 
-
-		// Acceptation du logIn (boolean true)
-		// Utilisation de la méthode de redirection
-		logInCl = true;
-		return "/secured/espace.xhtml?faces-redirect=true";
-	} catch (Exception ex) {
-		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Mail ou MdP Invalide"));
-		return "/login.xhtml";
+			// Acceptation du logIn (boolean true)
+			// Utilisation de la méthode de redirection
+			logInCl = true;
+			return "/secured/espace.xhtml?faces-redirect=true";
+		} catch (Exception ex) {
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Mail ou MdP Invalide"));
+			return "/login.xhtml";
+		}
 	}
-}
 
-public String logout() {
-	FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
-	return "/home.xhtml";
-}
+	public String logout() {
+		FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+		return "/home.xhtml";
+	}
 }
